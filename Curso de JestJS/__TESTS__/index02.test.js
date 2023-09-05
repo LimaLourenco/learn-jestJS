2d31dd4c2e3d5dcbb9bec1e3ce69622760b86803
andre lima <a.andrelima@outlook.com.br>
2023-09-04 22:24:54 -0300

    3. Curso de JestJS - Módulo II - Asserção toBeNull

---
+++ Curso de JestJS/__TESTS__/index02.test.js
@@ -0,0 +1,22 @@
+const PersonFactory = require('../index02');
+
+let factory = new PersonFactory(); // Criando um novo objeto, apartir da minha PersonFactory.
+
+const { expect, test, describe } = require('@jest/globals');
+
+describe("Factory of Person", () => {
+    // Definindo, quais serão os testes que vou colocar aqui:
+    test("Not passing the parameter should return null", () => { // Se eu não passar um parametro, espero tornar null.
+
+        expect(factory.getPerson()).toBeNull(); // Não passando nunhum parametro, o resultado/returno tem que ser null/nulo.
+    })
+
+    test("passed the invalid parameter admin should return null", () => { // Se eu passar um parametro inválido, ou seja tem que ser resultado/retorno null. 
+        expect(factory.getPerson("admin")).toBeNull();
+    })
+
+})
+
+// Observação 1: Para este teste passar, os dois resultados tem que ser null/nulo. 
+
+// Observação 2: Sendo assim, vai ter que receber no final das contas: o Objeto ou Null.
--- Curso de JestJS/index02.js
+++ Curso de JestJS/index02.js
@@ -1,1 +1,22 @@
 // Curso de JestJS - Módulo II - Asserção toBeNull
+
+const {Employee, Client} = require('../Curso de JestJS/files/person');
+
+// Está minha classe vai ter um método, que vai retorna uma pessoa.
+
+module.exports = class PersonFactory {
+    getPerson(type) {
+      if (type == null) {
+        return null;
+      }
+  
+      switch (type.toLowerCase()) {
+        case "employee":
+          return new Employee();
+        case "client":
+          return new Client();
+        default:
+          return null;
+      }
+    }
+  };
