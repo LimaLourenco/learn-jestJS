12634a6d9564bb60aff40d94528f140aae6511ab
andre lima <a.andrelima@outlook.com.br>
2023-09-11 21:45:40 -0300

    Ajustes e update em relação ao Módulo II - Asserção toBeInstanceOf

--- Curso de JestJS/__TESTS__/index03.test.js
+++ Curso de JestJS/__TESTS__/index03.test.js
@@ -1,33 +1,37 @@
 // Curso de JestJS - Módulo II - Asserção toBeInstanceOf
 
-// Vai verificar se por acaso, existe uma referencia, que seja da instancia Cliente (Que cria um novo objeto).
-// Ou caso, contrário uma instancia de Funcionário.
-// Se passou na palavra client retorna o objeto isClient, e se passar na palavra employee retorna o objeto funcionário -> isEmployee.
+// Irá verificar se por acaso, existe uma referencia, que seja da instancia isCliente (Cria um novo objeto sendo cliente).
 
-const {isClient, isEmployee} = require('../files/personIndex03');
+// Ou caso, contrário uma instancia de isEmployee.
 
-const PersonFactory = require('../index03');
+// Se passar na palavra client retorna o objeto isClient, e se passar na palavra employee retorna o objeto funcionário => isEmployee.
+
+const {isClient, isEmployee} = require('../files/personIndex03'); // Importando os dois Objetos.
+
+const PersonFactory = require('../index03'); 
 
 const { describe, test, expect } = require('@jest/globals');
 
-// Tendo acesso ao Objeto principal, para crição de outros objetos:
+// Tendo acesso ao Objeto principal, para criação de outros objetos:
 let factory = new PersonFactory();
 
 // Descrevendo o que a gente pretende fazer: 
 describe("Factory of Persons", () => {
-    // Se a primeira referencia retornada vai ser um cliente e a segunda um funcionário,
-    // caso contrário não for um objeto dessas classes, eu vou retornar que não passou no teste.
+    // Se a primeira referencia/objeto retornada vai ser um cliente, e a segunda vai ser um funcionário,
+    // caso o contrário não for um objeto dessas classes, eu vou retornar que não passou no teste.
     test("passed a string with the client value, it should return a new client object", () => {
         expect(factory.getPerson("client")).toBeInstanceOf(isClient);
-        // O Client que é um objeto que foi importado. Ou seja, tem que ser uma instancia deste tipo de classe Client.
+        // *Passou na palavra client, ele é o objeto isClient. E tem que ser uma instancia da classe isClient.
     })
     test("passed a string with the employee value, it should return a new employee object", () => {
         expect(factory.getPerson("employee")).toBeInstanceOf(isEmployee);
     })
 })
 
-// Observação 1: Só vai passar esses testes, caso as duas instancias (Para criar um novo objeto) estejam corretas,
-// por exemplo, se eu passar um string "employee", eu quero que seja uma instancia (toBeInstanceOf) de um objeto Employee.
+// Observação 1: Só vai passar esses testes, caso as duas instancias (cria um novo objeto) estejam corretas,
+// então por exemplo, se eu passar um string "employee", eu quero que seja uma instancia -> (toBeInstanceOf) de um objeto Employee.
 
-// Observação 2: Se eu precisa, por algum motivo validar alguma referencia de objeto, uma instancia de objeto,
-// para verificar se está instancia foi criada apartir de uma classe determinada, eu posso usar o toBeInstanceOf para verificar a origem deste objeto, e que instancia ele é, (Ou seja, quem está criando um novo objeto).
+// Observação 2: Se eu preciso, por algum motivo validar alguma uma instancia de objeto,
+// preciso verificar, se está instancia foi criada a partir de uma classe determinada, 
+// e assim eu posso usar o toBeInstanceOf para verificar a origem deste objeto,
+// e que instancia ele é, (Ou seja, quem será que está criando o novo objeto).
